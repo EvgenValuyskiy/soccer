676f3b12e0ced39ee184b32791d5250d1aceb80b
volina <evgen.valuyskiy@gmail.com>
2018-04-17 07:56:44 +0300

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { ScoreComponent } from './score/score.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ScoreComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
